9861103dc4374f545f248674224ad7c28f7912a1
dev_zach <zmohan@wustl.edu>
2025-01-05 17:20:47 -0500

    Updating geo methods

--- autocv/utils/__init__.py
+++ autocv/utils/__init__.py
@@ -1,6 +1,6 @@
 """This package provides utilities for sequence and iterable filtering."""
 
-__all__ = ("find_first", "get_center", "get_first")
+__all__ = ("find_first", "get_center", "get_first", "get_random_point")
 
 from .filtering import find_first, get_first
-from .geometry import get_center
+from .geometry import get_center, get_random_point
