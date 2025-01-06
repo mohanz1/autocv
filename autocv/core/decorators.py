19d3687f074eb5c8bf645dd777c2af8810699fe4
dev_zach <zmohan@wustl.edu>
2025-01-05 20:06:01 -0500

    Cleaning up decorators

--- autocv/core/decorators.py
+++ autocv/core/decorators.py
@@ -10,8 +10,9 @@ from __future__ import annotations
 __all__ = ("check_valid_hwnd", "check_valid_image")
 
 import functools
-from typing import TYPE_CHECKING, Any, ParamSpec, TypeVar, cast
+from typing import TYPE_CHECKING, Concatenate, ParamSpec, TypeVar, cast
 
+from autocv.core import Vision, WindowCapture
 from autocv.models import InvalidHandleError, InvalidImageError
 
 if TYPE_CHECKING:
@@ -19,9 +20,11 @@ if TYPE_CHECKING:
 
 P = ParamSpec("P")
 R = TypeVar("R")
+WindowCaptureT = TypeVar("WindowCaptureT", bound=WindowCapture)
+VisionT = TypeVar("VisionT", bound=Vision)
 
 
-def check_valid_hwnd(func: Callable[..., R]) -> Callable[P, R]:
+def check_valid_hwnd(func: Callable[Concatenate[WindowCaptureT, P], R]) -> Callable[Concatenate[WindowCaptureT, P], R]:
     """Decorator that checks if `self.hwnd` exists and is not -1.
 
     Raises:
@@ -29,16 +32,18 @@ def check_valid_hwnd(func: Callable[..., R]) -> Callable[P, R]:
     """
 
     @functools.wraps(func)
-    def wrapper(self: Any, *args: P.args, **kwargs: P.kwargs) -> R:
+    def wrapper(self: WindowCaptureT, *args: P.args, **kwargs: P.kwargs) -> R:
         # Just check if 'hwnd' exists and is not -1
         if not hasattr(self, "hwnd") or self.hwnd == -1:
             raise InvalidHandleError(self.hwnd)
         return func(self, *args, **kwargs)
 
-    return cast("Callable[P, R]", wrapper)
+    wrapped = functools.wraps(func)(wrapper)
+    # Cast the wrapped function to the correct type so Mypy stops complaining
+    return cast("Callable[Concatenate[WindowCaptureT, P], R]", wrapped)
 
 
-def check_valid_image(func: Callable[..., R]) -> Callable[P, R]:
+def check_valid_image(func: Callable[Concatenate[VisionT, P], R]) -> Callable[Concatenate[VisionT, P], R]:
     """Decorator that checks if `self.opencv_image` exists and is non-empty.
 
     Raises:
@@ -46,10 +51,12 @@ def check_valid_image(func: Callable[..., R]) -> Callable[P, R]:
     """
 
     @functools.wraps(func)
-    def wrapper(self: Any, *args: P.args, **kwargs: P.kwargs) -> R:
+    def wrapper(self: VisionT, *args: P.args, **kwargs: P.kwargs) -> R:
         # Just check if 'opencv_image' exists and is non-empty
         if not hasattr(self, "opencv_image") or self.opencv_image.size == 0:
             raise InvalidImageError
         return func(self, *args, **kwargs)
 
-    return cast("Callable[P, R]", wrapper)
+    wrapped = functools.wraps(func)(wrapper)
+    # Cast the wrapped function to the correct type so Mypy stops complaining
+    return cast("Callable[Concatenate[VisionT, P], R]", wrapped)
